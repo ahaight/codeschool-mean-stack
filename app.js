107b506801a1c7315bc577b7a3b1f782867db119
Aaron Haight <ahaight1013@gmail.com>
2017-01-07 13:53:55 -0500

    added images for dodecahedron and pentagonal gem

--- app.js
+++ app.js
@@ -10,12 +10,22 @@
       name: "Dodecahedron",
       price: 2.95,
       description: "The Dodecahedron has been a source of metaphysical interest for at least 2000 years. Like a crystal or gem, its facets and symmetries compel our eyes and hearts to observe life more deeply.",
+      images: [
+      {
+        full: "dodecahedron-01-full.jpg",
+      }
+      ],
       canPurchase: true,
     },
     {
       name: "Pentagonal Gem",
       price: 5.95,
       description: "A Pentagonal Gem has a pentagon as a cross section or as a base",
+      images: [
+      {
+        full: "pentagonal-gem-01-full.jpg",
+      }
+      ],
       canPurchase: false,
     }
 ];
