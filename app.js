556c289583910bd8b8861bdb9df3ea9ec14e188a
Aaron Haight <ahaight1013@gmail.com>
2019-09-28 19:26:46 -0400

    added gallery controller

--- app.js
+++ app.js
@@ -16,6 +16,13 @@
     return this.tab === checkTab;
   };
 
+  app.controller('GalleryController', function(){
+    this.current = 0;
+    this.setCurrent = function(newGallery){
+      this.current = newGallery || 0;
+    };
+  });
+
 });
 
   var gems = [
